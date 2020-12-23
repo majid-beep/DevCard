a29d160261dc29aedf47ee7d3909eb8eeffd0885
7 <7@GENERAL-MANAGER>
2020-12-23 17:22:14 +0330

    article moved to view components

--- DevCard_mvc/DevCard_mvc/Models/Article.cs
+++ DevCard_mvc/DevCard_mvc/Models/Article.cs
@@ -5,7 +5,15 @@
         public long Id { set; get; }
         public string Title { set; get; }
         public string Description { set; get; }
-       
+        public string Image { set; get; }
+
+        public Article(long id, string title, string description, string image)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Image = image;
+        }
     }
 }
 
---
+++ DevCard_mvc/DevCard_mvc/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using DevCard_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DevCard_mvc.ViewComponents
+{
+    public class LatestArticlesViewComponent: ViewComponent
+    {
+       public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>
+            {
+                new Article( id:1, title:" ویندوز ", description:"آموزش ویندورز" , image:"blog-post-thumb-card-1.jpg" ),
+                 new Article( id:2, title:"  ورد ", description:"آموزش ورد" , image:"blog-post-thumb-card-2.jpg" ),
+                  new Article( id:3, title:" اکسل ", description:"آموزش اکسل" , image:"blog-post-thumb-card-3.jpg" ),
+                   new Article( id:4, title:" سی شارپ ", description:"آموزش  سی شارپ" , image:"blog-post-thumb-card-4.jpg" ),
+
+            };
+            return View("_LatestArticles", articles);
+        }
+    }
+}
