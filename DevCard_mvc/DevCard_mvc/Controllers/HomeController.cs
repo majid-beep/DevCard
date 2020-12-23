41ede0e234d0e1a0deab5b7cd44a50b21e7c2d19
7 <7@GENERAL-MANAGER>
2020-12-23 12:36:57 +0330

    index&contact page added

--- DevCard_mvc/DevCard_mvc/Controllers/HomeController.cs
+++ DevCard_mvc/DevCard_mvc/Controllers/HomeController.cs
@@ -1,32 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using DevCard_mvc.Models;
 
 namespace DevCard_mvc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+      
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
+
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
