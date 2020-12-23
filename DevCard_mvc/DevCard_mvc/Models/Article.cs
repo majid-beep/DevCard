22300563d635dd5bbe13386d3a725fe6bbd3861b
7 <7@GENERAL-MANAGER>
2020-12-23 15:01:11 +0330

    project section moved to a partial view section

---
+++ DevCard_mvc/DevCard_mvc/Models/Article.cs
@@ -0,0 +1,11 @@
+﻿namespace DevCard_mvc.Models
+{
+    public class Article
+    {
+        public long Id { set; get; }
+        public string Title { set; get; }
+        public string Description { set; get; }
+       
+    }
+}
+
---
+++ DevCard_mvc/DevCard_mvc/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard_mvc.Models
+{
+    public class Project
+    {
+        public long Id { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string Client { set; get; }
+
+    }
+}
+
