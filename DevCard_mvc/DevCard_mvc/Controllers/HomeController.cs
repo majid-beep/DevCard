1ff133438fda104f4e1f3f52a0576bd06cec3579
7 <7@GENERAL-MANAGER>
2020-12-24 11:04:08 +0330

    controller side contact form validation implemented

--- DevCard_mvc/DevCard_mvc/Controllers/HomeController.cs
+++ DevCard_mvc/DevCard_mvc/Controllers/HomeController.cs
@@ -3,16 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 using DevCard_mvc.Models;
 using Microsoft.AspNetCore.Http;
 using System;
+using FormFactory;
 
 namespace DevCard_mvc.Controllers
 {
     public class HomeController : Controller
     {
-      
+
 
         public HomeController()
         {
-            
+
         }
 
         public IActionResult Index()
@@ -28,12 +29,19 @@ namespace DevCard_mvc.Controllers
         }
 
         [HttpPost]
-        public JsonResult Contact(Contact form)
+        public IActionResult Contact(Contact model)
         {
-            Console.WriteLine(form.ToString());
-            return Json(Ok());
+            if (!ModelState.IsValid)
+            {
+                ViewBag.error = "ارسال پیام شما به مشکل مواجه شد. لطفا دوباره تلاش کنید.";
+                return View(model);
+            }
+            ViewBag.success= "ایمیل شما با موفقیت ارسال شد. با تشکر.";
+            return View();
+
         }
-       
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
